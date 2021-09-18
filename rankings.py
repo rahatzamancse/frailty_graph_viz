e1fc9729c5e075a060a22795a6729f094680e6e8
Enrique Noriega <enrique.noriega@gmail.com>
2021-09-17 17:01:44 -0700

    Improved the ranking script by removing hard coded paths to my home directory

--- rankings.py
+++ rankings.py
@@ -1,12 +1,17 @@
 """ Make a dictionary with maps from PMCID -> journal name and journal name to impact metrics """
 
-from tqdm import tqdm
-import pandas as pd
 import csv
-import re
 import pickle
+import re
+from pathlib import Path
 
-def main(pmc_file_list = '/Users/enrique//Downloads/oa_file_list.csv', rankings_file = '/Users/enrique/Downloads/scimagojr 2020.csv', output_pickle = "journal_rankings.pickle"):
+import plac
+from tqdm import tqdm
+
+@plac.pos('pmc_file_list', help='Path to the FTP service file list', type=Path)
+@plac.pos('rankings_file', help='CSV file with the rankings and impact factor data', type=Path)
+@plac.pos('output_pickle', help='Output path for the pickle with the resulsts', type=Path)
+def main(pmc_file_list:Path = Path('data', 'oa_file_list.csv'), rankings_file = Path('data', 'scimagojr 2020.csv'), output_pickle = Path('data', "journal_rankings.pickle")):
 
 
     with open(pmc_file_list) as f:
@@ -37,9 +42,8 @@ def main(pmc_file_list = '/Users/enrique//Downloads/oa_file_list.csv', rankings_
     journal_keys = list(sorted(rankings.keys()))
 
     for j in tqdm(pmcoa_journals, desc='Matching journals to rankings', unit='entries'):
-        j = j.lower()
         for key in journal_keys:
-            if key.startswith(j):
+            if key.startswith(j.lower()):
                 maps.append((j, key))
                 data = rankings[key]
                 h = float(data['H index'])
@@ -55,10 +59,11 @@ def main(pmc_file_list = '/Users/enrique//Downloads/oa_file_list.csv', rankings_
 
     with open(output_pickle, 'wb') as f:
         pickle.dump({
+            'pmc_to_sjr': maps,
             'journals':pmcid_2_journal,
             'hindex': hindex,
             'sjr':sjr
         }, f)
 
 if __name__ == '__main__':
-    main()
+    plac.call(main)
