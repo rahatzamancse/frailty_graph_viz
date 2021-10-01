92e8236035416ccfc9aace458fe1d229b0990760
Enrique Noriega <enrique.noriega@gmail.com>
2021-10-01 14:32:55 -0700

    Added the equation as latex to the overview page

--- static/utils.js
+++ static/utils.js
@@ -15,6 +15,25 @@ function arizona_weight(d){
         avg_pval_w * (1-d['avg_pvalue'])
 }
 
+function arizona_weight_latex(){
+    const freq_w = readSliderValue("freq")
+    const has_sig_w = readSliderValue("has-sig")
+    const avg_sig_w = readSliderValue("avg-sig")
+    const avg_impact_w = readSliderValue("avg-impact")
+    const max_impact_w = readSliderValue("max-impact")
+    const avg_pval_w = readSliderValue("avg-pvalue")
+
+    let latex = `\\begin{equation}
+                        \\begin{aligned}
+                          \\textbf{weight} = &${freq_w}\\times\\log(\\text{freq} + 1) + ${has_sig_w} \\times \\text{has significance} + \\\\
+                            & ${avg_sig_w} \\times \\text{avg significance} +  ${avg_impact_w} \\times \\text{avg impact factor} + \\\\
+                            & ${max_impact_w} \\times \\text{max impact factor} +  ${avg_pval_w} \\times (1 - \\text{avg p-value}) \\\\
+                        \\end{aligned}
+                \\end{equation}`
+
+    return latex
+}
+
 function readSliderValue(name){
 
     const localStorage = window.localStorage
@@ -29,3 +48,4 @@ function setSliderValue(name, val){
     const localStorage = window.localStorage
     localStorage.setItem(`${name}-weight`, val)
 }
+
