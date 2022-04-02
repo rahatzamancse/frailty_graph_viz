7a72cfccf0ea18b2de07645a16987b8104316a44
Enrique Noriega <enrique.noriega@gmail.com>
2022-04-01 18:44:40 -0700

    Small bugfix

--- backend/build_network.py
+++ backend/build_network.py
@@ -54,10 +54,9 @@ def parse_file(p, bibliography: Mapping[str, XDDMetaData]):
                 if r['SEEN IN'] is not None:
                     # TODO: Fix this in the source
                     if r['SEEN IN'] == "PMC0":
-                        seen_in = Path(p).name.split('-')[0][3:]
-                        r['SEEN IN'] = seen_in
-                    else:
-                        seen_in = r['SEEN IN'][3:]
+                        r['SEEN IN'] = Path(p).name.split('-')[0]
+
+                    seen_in = r['SEEN IN'][3:]
                     if seen_in in bibliography:
                         metadata = bibliography[seen_in]
                         r['JOURNAL'] = metadata.journal
